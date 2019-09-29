0abdf93435e4beaa4853862feef616d48e558ccd
Latheesh <latheeshv90@gmail.com>
2019-09-29 18:47:14 -0500

    Automated Ui test

--- app/src/androidTest/java/latheesh/com/prestosignup/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/latheesh/com/prestosignup/ExampleInstrumentedTest.java
@@ -2,11 +2,17 @@ package latheesh.com.prestosignup;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +22,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+@Rule
+public ActivityTestRule<MainActivity>activityActivityTestRule=new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -23,4 +32,14 @@ public class ExampleInstrumentedTest {
 
         assertEquals("latheesh.com.prestosignup", appContext.getPackageName());
     }
+@Test
+    public void Signuptest()
+{
+    onView(withId(R.id.signup)).perform(click());
+    onView(withId(R.id.reg_firstName)).perform(typeText("John"));
+    onView(withId(R.id.reg_lastName)).perform(typeText("doe"));
+    onView(withId(R.id.reg_email)).perform(typeText("john.doe@gmail.com"));
+    onView(withId(R.id.reg_phonenum)).perform(typeText("12345567899"));
+    onView(withId(R.id.reg_register)).perform(click());
+}
 }
